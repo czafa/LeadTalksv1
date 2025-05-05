c1347e41343655584f8b511c28c07707eb9f24ba
czafa <caiozafa25@gmail.com>
2025-05-05 11:02:24 -0300

    fix: hook `seLeadTalks.ts

--- frontend/src/hooks/useLeadTalks.ts
+++ frontend/src/hooks/useLeadTalks.ts
@@ -1,10 +1,10 @@
-// Exemplo: src/hooks/useLeadTalk.ts
 import { supabase } from "../lib/supabase";
+
 export const iniciarLeadTalk = async () => {
   const session = await supabase.auth.getSession();
   const token = session.data.session?.access_token;
 
-  const res = await fetch("/api/iniciar-leadtalk", {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/iniciar-leadtalk`, {
     method: "POST",
     headers: {
       Authorization: `Bearer ${token}`,
