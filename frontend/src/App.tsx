8b08b9c9cad602a415e194ef31b5297ca2be644e
czafa <caiozafa25@gmail.com>
2025-05-14 18:30:44 -0300

    fix: `app.tsx`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -13,25 +8,25 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* redireciona / para /login */}
-        <Route path="/" element={<Navigate to="/login" />} />
+    <Routes>
+      {/* Redireciona / para /login */}
+      <Route path="/" element={<Navigate to="/login" />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/recover" element={<Recover />} />
-        <Route path="/qr" element={<QR />} />
+      {/* Rotas públicas */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/recover" element={<Recover />} />
+      <Route path="/qr" element={<QR />} />
 
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+      {/* Rota protegida */}
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
