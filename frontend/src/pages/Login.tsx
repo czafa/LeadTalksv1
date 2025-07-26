9a50a1e71ca88f9cc763b8d3c0833836c37d508a
czafa <caiozafa25@gmail.com>
2025-07-26 19:36:00 -0300

    fix(cors): add new `vercel.js` config 1 teste login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -38,19 +38,19 @@ export default function Login() {
 
     const BACKEND_URL = import.meta.env.VITE_API_URL;
 
-    await fetch(`${BACKEND_URL}/api/sessao`, {
-      // envia para o /backend o json com o usuario_id, logado e conectado.
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        usuario_id,
-        logado: true,
-        conectado: false,
-      }),
-    });
+    // await fetch(`${BACKEND_URL}/api/sessao`, {
+    //   // envia para o /backend o json com o usuario_id, logado e conectado.
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    //   body: JSON.stringify({
+    //     usuario_id,
+    //     logado: true,
+    //     conectado: false,
+    //   }),
+    // });
 
     navigate("/qr");
   };
